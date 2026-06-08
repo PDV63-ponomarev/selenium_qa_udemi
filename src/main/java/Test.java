0ea9976e0f32d8de4eca69eaf2c2882beb026500
ponomarev.dv <graiwers@gmail.com>
2026-06-08 16:49:28 +0400

    les5_2

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
+
 public class Test {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
@@ -10,27 +13,29 @@ public class Test {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://google.com");
-
-        WebElement input = driver.findElement(
-                By.cssSelector(".truncate")
-        );
-
-        WebElement input2 = driver.findElement(
-                By.xpath("//*[@class=\"truncate\"]")
-        );
-
-        WebElement input3 = driver.findElement(
-                By.className("truncate")
-        );
+//
+//        WebElement input = driver.findElement(
+//                By.cssSelector(".truncate")
+//        );
+//
+//        WebElement input2 = driver.findElement(
+//                By.xpath("//*[@class=\"truncate\"]")
+//        );
+//
+//        WebElement input3 = driver.findElement(
+//                By.className("truncate")
+//        );
 
 
 //        Неявное ожидание
 //        ожидание на поиск
-        driver.manage().timeout().implicitlyWait(10, TimeUit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 //        время на загрузку страницы
-        driver.manage().timeout().pageLoadTimeout(10, TimeUit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 //        ожидания завершения выполнения асинхронного скрипта
-        driver.manage().timeout().setScriptTimeout(10, TimeUit.SECONDS);
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
 
+
+//
     }
 }
---
+++ src/main/java/les6.java
@@ -0,0 +1,4 @@
+package main.java;
+
+public class les6 {
+}
